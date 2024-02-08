fcd3e7895a882199ba5d46d31c1329afc9bedb53
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-02-09 03:56:23 +0800

    feat: add verbose error logging via Display trait for enum ArgError

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use core::result::Result;
+use std::fmt::Display;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -10,9 +11,19 @@ struct Args {
 
 #[derive(Debug, PartialEq)]
 enum ArgError {
-    NonASCII,
-    NonAlphabetic,
-    NonLowercase,
+    NonASCII(char),
+    NonAlphabetic(char),
+    NonLowercase(char),
+}
+
+impl Display for ArgError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NonASCII(c) => write!(f, "{c} is not an ASCII character"),
+            Self::NonAlphabetic(c) => write!(f, "{c} is not an alphabetic character"),
+            Self::NonLowercase(c) => write!(f, "{c} is not a lowercase alphabetical character"),
+        }
+    }
 }
 
 fn main() {
@@ -28,11 +39,11 @@ fn lettersum(s: &str) -> Result<u32, ArgError> {
     let mut chars = s.chars();
     while let Some(x) = chars.next() {
         if !x.is_ascii() {
-            return Err(ArgError::NonASCII);
+            return Err(ArgError::NonASCII(x));
         } else if !x.is_alphabetic() {
-            return Err(ArgError::NonAlphabetic);
+            return Err(ArgError::NonAlphabetic(x));
         } else if x.is_uppercase() {
-            return Err(ArgError::NonLowercase);
+            return Err(ArgError::NonLowercase(x));
         }
     }
     Ok(s.chars().map(|x: char| {x as u32 - 'a' as u32 + 1}).sum())
@@ -43,8 +54,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_invalid() {
-        assert_eq!(lettersum("Hello"), Err(ArgError::NonLowercase));
+    fn test_non_lowercase() {
+        assert_eq!(lettersum("Hello"), Err(ArgError::NonLowercase('H')));
+    }
+
+    #[test]
+    fn test_non_ascii() {
+        assert_eq!(lettersum("ß"), Err(ArgError::NonASCII('ß')));
+    }
+
+    #[test]
+    fn test_non_alphabetic() {
+        assert_eq!(lettersum("123"), Err(ArgError::NonAlphabetic('1')));
     }
 
     #[test]
